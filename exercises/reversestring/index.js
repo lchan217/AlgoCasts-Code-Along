ed0457f9537d6f4c39a6f56e67ee81effdb6565c
Lizzie <lchan217@gmail.com>
2021-01-27 09:49:14 -0700

    reversestring completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // SOLUTION 1
+    // return str.split('').reverse().join('')
+
+    // SOLUTION 2
+    // let result = ''
+    // for(let char of str){
+    //     result = char + result
+    // }
+    // return result
+
+    // LC: SOLUTION 3 
+    return str.split('').reduce((reversed, character) =>
+        character + reversed
+    , '');
+}
+
+reverse('bear');
 
 module.exports = reverse;
