bd6b95a9a4a9bbfecbc0dc80c935ae5375f4983c
Lizzie <lchan217@gmail.com>
2021-01-28 17:49:49 -0700

    maxchar completed

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const hash = {}
+
+    // for(let char of str){
+    //     if (hash[char]){
+    //         hash[char]++
+    //     } else {
+    //         hash[char] = 1
+    //     }
+    // }
+
+    for(let char of str){
+        hash[char] = hash[char] + 1 || 1
+    }
+    
+    let max = 0
+    let maxChar = ''
+    for(let char in hash){
+        if(hash[char] > max){
+            max = hash[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
