b7b2d049612e523964cea35d7de7eb4215e45929
Lizzie <lchan217@gmail.com>
2021-01-29 09:45:27 -0700

    chunk and fizzbuzz complete

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size){
+    // LC: SOLUTION 1
+    // const result = [];
+  
+    // for(let n of array){
+    //   const last = result[result.length - 1]
+    //   if(!last || last.length === size){
+    //     result.push([n])
+    //   } else {
+    //     last.push(n)
+    //   }
+    // }
+
+    // return result
+
+    // LC: SOLUTION 2
+    const result = []
+    let index = 0
+
+    while(index < array.length){
+
+        result.push(array.slice(index, index + size))
+        index += size
+    }
+
+    return result
+}
 
 module.exports = chunk;
