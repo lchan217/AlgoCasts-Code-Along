b12fa2d409c5453a3cf5fb1d9d80eb447a201933
Lizzie <lchan217@gmail.com>
2021-02-11 09:30:14 -0700

    add fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,59 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+    // My SOLUTION
+    // const array = [0, 1]
+    // for(let i = 1; i <= n; i++){
+    //     let sum = array[i-1] + array[i]
+    //     array.push(sum)
+    // }
+    // return array[n]
+
+    // SOLUTION 1
+    // const result = [0,1]
+
+    // for(let i = 2; i <= n; i++){
+    //     const a = result[i-1]
+    //     const b = result[i-2]
+    //     result.push(a+b)
+    // }
+    // return result[n]
+
+    // SOLUTION 2 - exponential!!
+    // fib is being called multiple times with same argument
+    // memoization! - store arguments and its result, so you can return 
+    // precomputed result instead of running function again
+    
+    // if(n < 2){
+    //     return n
+    // }
+
+    // return fib(n-1) + fib(n-2)
+// }
+
+// SOLUTION 3 - memoized
+function memoize(fn){
+    const cache = {}
+    return function(...args){
+        if(cache[args]){
+            return cache[args]
+        }
+
+        const result = fn.apply(this, args)
+        cache[args] = result
+        return result
+    }
+}
+
+function slowFib(n){
+    if(n < 2){
+        return n
+    }
+
+    return fib(n-1) + fib(n-2)
+}
+
+const fib = memoize(slowFib)
 
 module.exports = fib;
