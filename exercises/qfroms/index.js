42af3992eddb1f86a485152dcfde6622202c1c13
Lizzie <lchan217@gmail.com>
2021-02-11 10:48:04 -0700

    queues and stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -1,3 +1,5 @@
+// queue from stack
+
 // --- Directions
 // Implement a Queue datastructure using two stacks.
 // *Do not* create an array inside of the 'Queue' class.
@@ -13,7 +15,49 @@
 //     q.remove(); // returns 2
 
 const Stack = require('./stack');
+// stack 1 => stack 2 => new queue
+class Queue {
+    constructor(){
+        this.first = new Stack();
+        this.second = new Stack();
+    }
 
-class Queue {}
+    // add to first stack
+    add(record){
+        this.first.push(record)
+    }
+
+    // move everything to the second stack to 
+    // get to the bottom of the first stack so you can 
+    // pop it off
+    // then move everything back to first stack to restore
+    // in order for next operation
+    remove(){
+        while(this.first.peek()){
+            this.second.push(this.first.pop())
+        }
+        const record = this.second.pop()
+
+        while(this.second.peek()){
+            this.first.push(this.second.pop())
+        }
+
+        return record
+    }
+    
+    // peek and restore to first stack
+    peek(){
+        while(this.first.peek()){
+            this.second.push(this.first.pop())
+        }
+
+        const record = this.second.peek()
+
+        while(this.second.peek()){
+            this.first.push(this.second.pop())
+        }
+        return record
+    }
+}
 
 module.exports = Queue;
